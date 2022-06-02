8dc9a5ebe735d5d3a68519a0e070dea8dcb293a7
Sun <79148947+dyeong-00@users.noreply.github.com>
2022-06-02 14:21:28 +0900

    2022.06.02.

--- LV2/RotateBracket.py
+++ LV2/RotateBracket.py
@@ -2,10 +2,11 @@
 
 def solution(s):
     answer = 0
+    s = list(s)
     for i in range(0, len(s)):
         answer += iscorrect(s)
         #print(s, "\n")
-        s = s[1:] + s[0]
+        s.append(s.pop(0))
     return answer
 
 def iscorrect(s):
@@ -14,28 +15,22 @@ def iscorrect(s):
         if s[i] == '[' or s[i] == '{' or s[i] == '(':
             l.append(s[i])
         elif s[i] == ']':
-            if len(l) != 0:
-                if l[-1] == '[':
-                    l.pop()
-                    continue
-                else:
-                    return 0
+            if (len(l) != 0) and (l[-1] == '['):
+                l.pop()
+                continue
             else: return 0
         elif s[i] == '}':
-            if len(l) != 0:
-                if l[-1] == '{':
-                    l.pop()
-                    continue
-                else:
-                    return 0
+            if (len(l) != 0) and (l[-1] == '{'):
+                l.pop()
+                continue
             else: return 0
         elif s[i] == ')':
-            if len(l) != 0:
-                if l[-1] == '(':
-                    l.pop()
-                    continue
-                else:
-                    return 0
+            if (len(l) != 0) and (l[-1] == '('):
+                l.pop()
+                continue
             else: return 0
-
-    return 1
+    # 중요! l이 비어있는 경우에만 1이 되도록
+    if (len(l) == 0):
+        return 1
+    else:
+        return 0
