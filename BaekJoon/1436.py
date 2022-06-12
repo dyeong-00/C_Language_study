b5df294f0d5003e915e2d8b4013327984feb5560
Sun <79148947+dyeong-00@users.noreply.github.com>
2022-06-12 11:26:04 +0900

    2022.06.12.

---
+++ BaekJoon/1436.py
@@ -0,0 +1,12 @@
+# 영화감독 숌
+
+N = int(input())
+ans = 666
+while(True):
+    if '666' in str(ans):
+        if N == 1:
+            print(ans)
+            break
+        else:
+            N -= 1
+    ans += 1
